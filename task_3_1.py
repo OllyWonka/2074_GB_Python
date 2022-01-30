dca6674bdc5779a95c572e86257866ab1c19c0ed
Olga <84392330+OllyWonka@users.noreply.github.com>
2022-01-30 15:42:36 +0300

    Create task_3_1.py

---
+++ task_3_1.py
@@ -0,0 +1,20 @@
+def num_translate(value: str) -> dict:
+    dictionary = {
+        'zero': 'ноль',
+        'one': 'один',
+        'two': 'два',
+        'three': 'три',
+        'four': 'четыре',
+        'five': 'пять',
+        'six': 'шесть',
+        'seven': 'семь',
+        'eight': 'восемь',
+        'nine': 'девять',
+        'ten': 'десять'
+    }
+    str_out = dictionary.get(value)
+    return str_out
+
+
+print(num_translate("one"))
+print(num_translate("eight"))

97452c38418a510667a0b07b50ec48efeb0cba71
Olga <84392330+OllyWonka@users.noreply.github.com>
2022-02-05 20:39:13 +0300

    Update task_5_5.py

--- task_5_5.py
+++ task_5_5.py
@@ -1,9 +1,9 @@
-def get_numbers(src: list):
-    for num in range(len(src) - 1):
-        if src[num] < src[num + 1]:
-            yield src[num + 1]
-        pass
+def get_uniq_numbers(src: list):
+    for num in range(len(src)):
+        if src.count(num) == 1:
+            yield src[num]
+            pass
 
 
-src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
-print(*get_numbers(src))
+src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+print(*get_uniq_numbers(src))
